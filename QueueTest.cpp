50595df09f9423b35fd69c1ac6871ae25853cbba
Vishwas Srivastava <friendsandvis@gmail.com>
2026-07-12 16:04:13 +0530

    completed queuetest app.

--- QueueTest.cpp
+++ QueueTest.cpp
@@ -1,6 +1,39 @@
 #include"QueueTest.h"
-
 void QueueTest::Run()
 {
+	//add 21 elements to queue in 2 threads parallely all display until que uis empty in another thread.
+	std::thread adder1(&QueueTest::AddTask, this, 1, 10);
+	std::thread adder2(&QueueTest::AddTask, this, 50, 60);
+	m_threadsInAction.push_back(std::move(adder1));
+	m_threadsInAction.push_back(std::move(adder2));
+	std::thread displayQueue(&QueueTest::DisplayTask, this);
+	m_threadsInAction.push_back(std::move(displayQueue));
+	for (thread& t : m_threadsInAction)
+	{
+		t.join();
+	}
+}
 
+void QueueTest::AddTask(int startNum, int endNum)
+{
+	std::this_thread::sleep_for(chrono::milliseconds(10));
+	for(int i= startNum; i <= endNum; i++)
+	{
+		m_testQueue.Push(i);
+	}
+}
+
+void QueueTest::DisplayTask()
+{
+	std::this_thread::sleep_for(chrono::milliseconds(100));
+	while (!m_testQueue.ISEmpty())
+	{
+		int element;
+		bool popSuccess = m_testQueue.Pop(element);
+		if (popSuccess)
+		{
+			std::cout << element << std::endl;
+			std::this_thread::sleep_for(chrono::milliseconds(10));
+		}
+	}
 }
--- QueueTest.h
+++ QueueTest.h
@@ -4,12 +4,58 @@
 #include<shared_mutex>
 #include<mutex>
 #include<vector>
+#include<queue>
 #include<iostream>
+
+template<typename T>
+class ThreadSafeQueue
+{
+public:
+	ThreadSafeQueue()
+	{
+
+	}
+	ThreadSafeQueue(const ThreadSafeQueue& other) = delete;
+	ThreadSafeQueue(const ThreadSafeQueue&& other) = delete;
+	void Push(T& entry)
+	{
+		std::lock_guard<std::mutex> lock(m_mtx);
+		m_queue.push(entry);
+	}
+	bool Pop(T& popedEntry)
+	{
+		std::lock_guard<std::mutex> lock(m_mtx);
+		if (!m_queue.empty())
+		{
+			popedEntry = m_queue.front();
+			m_queue.pop();
+			return true;
+		}
+		return false;
+	}
+	bool ISEmpty()
+	{
+		std::lock_guard<std::mutex> lock(m_mtx);
+		return m_queue.empty();
+	}
+	size_t GetElementCount()
+	{
+		std::lock_guard<std::mutex> lock(m_mtx);
+		return m_queue.size();
+	}
+private:
+
+	std::mutex m_mtx;
+	std::queue<T> m_queue;
+};
 class QueueTest : public ThreadTest
 {
 public:
 	void Run() override;
+	void AddTask(int startNum, int endNum);
+	void DisplayTask();
 private:
 	static mutex printMtx;
 	vector<thread> m_threadsInAction;
+	ThreadSafeQueue<int> m_testQueue;
 };
